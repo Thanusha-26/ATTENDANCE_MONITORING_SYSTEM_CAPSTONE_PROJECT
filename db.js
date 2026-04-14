cb900394cf00267d645c5b3875e12d5d47081051
Thanusha-26 <thanusha2765@gmail.com>
2026-04-14 14:55:10 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mydatabase");
+    console.log("MongoDB Connected ✅");
+  } catch (error) {
+    console.log("DB Error:", error);
+    process.exit(1);
+  }
+};
